30ab6f726d4a5170c3cc037368805980144e21bf
andersons-tuche23 <134718682+andersons-tuche23@users.noreply.github.com>
2023-09-30 17:33:35 -0300

    casas

--- src/componente/Personagens/index.js
+++ src/componente/Personagens/index.js
@@ -1,10 +1,72 @@
+import React, { useEffect, useState } from 'react';
 import './Personagens.css';
 import { Link } from 'react-router-dom';
 import TextoNavegue from '../TextoNavegue';
-import PersonagensList from '../../PersonagensList';
-
+import PersonagensList from '../PersonagensList/PersonagensList';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 
 function Personagens() {
+
+    
+  const location = useLocation();
+  const queryParams = queryString.parse(location.search);
+  const casa = queryParams.casa;
+
+  const [personagens, setPersonagens] = useState([]);
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+
+  useEffect(() => {
+
+    if (casa) {
+
+      fetch(`https://hp-api.onrender.com/api/characters/house/${casa}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setPersonagens(data);
+        })
+        .catch((error) => {
+          console.error('Erro ao buscar personagens:', error);
+        });
+    } else {
+
+      fetch('https://hp-api.onrender.com/api/characters')
+        .then((response) => response.json())
+        .then((data) => {
+          setPersonagens(data);
+        })
+        .catch((error) => {
+          console.error('Erro ao buscar personagens:', error);
+        });
+    }
+  }, [casa]);
+
+
+  const openModal = (character) => {
+    setSelectedCharacter(character);
+  };
+
+  const closeModal = () => {
+    setSelectedCharacter(null);
+  };
+
+  const houses = () => { // Certifique-se de passar 'casa' como argumento para a função
+    switch (casa) {      
+      case "gryffindor":
+        return "Grifinória";
+      case "hufflepuff":
+        return "Lufa-Lufa";
+      case "ravenclaw":
+        return "Corvinal";
+      case "slytherin":
+        return "Sonserina";
+  
+      default:
+        return "PERSONAGENS"; // Adicionado 'return' aqui
+    }
+  };
+
+  console.log(casa, 'casa');
 
     return (
         <>
@@ -16,11 +78,11 @@ function Personagens() {
                         </Link>
                     </div>
                     <div>
-                        <TextoNavegue texto='Personagens' />
+                        <TextoNavegue texto={houses()} />
                     </div>
                 </div>
                 <div>
-                    <PersonagensList />
+                    <PersonagensList personagens={personagens} openModal={openModal} closeModal={closeModal} selectedCharacter={selectedCharacter} />
                 </div>
             </section>
 
---
+++ src/componente/PersonagensList/PersonagensList.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import './PersonagensList.css'
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+import Modal from '../Modal/index'
+
+
+function PersonagensList({personagens, closeModal, openModal, selectedCharacter}) {
+
+  return (
+    <>
+
+      <div className='meus-personagens'>
+        <div className="personagens-container">
+          {personagens.map(personagem => (
+            <div key={personagem.id} className="personagem" onClick={() => openModal(personagem)}>
+              <img src={personagem.image ? personagem.image : "/sem.imagem.png"} alt={personagem.name} />
+              <p className='nome-pesonagem'>{personagem.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {selectedCharacter && (
+        <Modal
+          isOpen={true}
+          onClose={closeModal}
+          character={selectedCharacter}
+        />
+      )}
+    </>
+
+  );
+}
+
+export default PersonagensList;
