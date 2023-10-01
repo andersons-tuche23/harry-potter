d2dd72660280cc7e614122e6ee311c040037a99c
andersons-tuche23 <134718682+andersons-tuche23@users.noreply.github.com>
2023-10-01 13:24:56 -0300

    harry-01-10

--- src/componente/Modal/index.js
+++ src/componente/Modal/index.js
@@ -18,18 +18,18 @@ const Modal = ({ isOpen, onClose, character }) => {
 
         <div className='container-modal'>
           <div className="modal-content">
-            <p><span>Nome:</span> {name}</p>
-            <p><span>Casa:</span> {house}</p>
-            <p><span>Espécie:</span> {specie}</p>
-            <p><span>Data de nascimento:</span> {dateOfBirth}</p>
-            <p><span>Gênero:</span> {male}</p>
-            <p><span>Patronus:</span> {patronus}</p>
+            <p><span>Nome:</span> {name ? name : "Não informado"}</p>
+            <p><span>Casa:</span> {house ? house : "Não informado"}</p>
+            <p><span>Espécie:</span> {specie ? specie : "Não informado"}</p>
+            <p><span>Data de nascimento:</span> {dateOfBirth ? dateOfBirth : "Não informado"}</p>
+            <p><span>Gênero:</span> {male ? male : "Não informado"}</p>
+            <p><span>Patronus:</span> {patronus ? patronus : "Não informado"}</p>
             <p><span>Varinha:</span></p>
-            <p><span>- Madeira:</span>{wood}</p>
-            <p><span>- Nucleo:</span> {core}</p>
-            <p><span>- Comprimento:</span> {length}</p>
-            <p><span>Ancestrabilidade:</span> {ancestry}</p>
-            <p><span>Ator:</span> {actor}</p>
+            <p><span>- Madeira:</span>{wood ? wood : "Não informado"}</p>
+            <p><span>- Nucleo:</span> {core ? core : "Não informado"}</p>
+            <p><span>- Comprimento:</span> {length ? length : "Não informado"}</p>
+            <p><span>Ancestrabilidade:</span>  {ancestry ? ancestry : "Não informado"}</p>
+            <p><span>Ator:</span> {actor ? actor : "Não informado"}</p>
           </div>
           <div>
             <img
